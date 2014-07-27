4cf13d492e59b5bed2362c270f0fd774f2e53b8c
Erik Aybar <erikthedeveloper@gmail.com>
2014-07-27 03:50:30 -0600

    Kickstart pages/view related files
    - Adds views
        - `layouts.default`
        - `pages.home`
    - Adds controller `PagesController`
    - Removes `hello.blade.php`

---
+++ app/views/layouts/default.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Larabook</title>
+
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+</head>
+<body>
+
+    <div class="container">
+        @yield('content')
+    </div>
+
+    <script type="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</body>
+</html>
