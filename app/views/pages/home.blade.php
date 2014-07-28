d17cf9fa1b6828de89e54e34f872387f87136acd
Erik Aybar <erikthedeveloper@gmail.com>
2014-07-28 07:06:22 -0600

    Adds `app/assets/sass` directory

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.default')
 
 @section('content')
-Foo
+<div class="jumbotron">
+    <h1>LaraBook <small>the place for developers</small></h1>
+    <p>
+        Welcome to <strong>LaraBook</strong>! The place for LaraDevelopers.
+    </p>
+    <p>
+        Take a look around...
+    </p>
+    <p>
+        <a href="#" class="btn btn-lg btn-primary" role="button">Sign Up</a>
+    </p>
+</div>
 @endsection('content')
